8fe49b79277587de7f2a992ce106731220202da6
Alex Taradov <alex@taradov.com>
2016-10-04 19:58:24 -0700

    Added SPI master implementation

---
+++ cli/spi.c
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2016, Alex Taradov <alex@taradov.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*- Includes ----------------------------------------------------------------*/
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include "spi.h"
+#include "dgw.h"
+#include "main.h"
+
+/*- Definitions -------------------------------------------------------------*/
+enum
+{
+  CMD_SPI_INIT     = 0x10,
+  CMD_SPI_SS       = 0x11,
+  CMD_SPI_TRANSFER = 0x12,
+};
+
+/*- Implementations ---------------------------------------------------------*/
+
+//-----------------------------------------------------------------------------
+int spi_init(int freq)
+{
+  uint8_t buf[10];
+
+  buf[0] = CMD_SPI_INIT;
+  buf[1] = (freq >> 0) & 0xff;
+  buf[2] = (freq >> 8) & 0xff;
+  buf[3] = (freq >> 16) & 0xff;
+  buf[4] = (freq >> 24) & 0xff;
+  dgw_cmd(buf, sizeof(buf), 5);
+
+  return ((uint32_t)buf[4] << 24) | ((uint32_t)buf[3] << 16) | ((uint32_t)buf[2] << 8) | buf[1];
+}
+
+//-----------------------------------------------------------------------------
+void spi_ss(int state)
+{
+  uint8_t buf[10];
+
+  buf[0] = CMD_SPI_SS;
+  buf[1] = state;
+  dgw_cmd(buf, sizeof(buf), 2);
+}
+
+//-----------------------------------------------------------------------------
+void spi_transfer(uint8_t *wdata, uint8_t *rdata, int size)
+{
+  uint8_t buf[100];
+  int offs = 0;
+
+  while (size)
+  {
+    int sz = (size > 62) ? 62 : size;
+
+    buf[0] = CMD_SPI_TRANSFER;
+    buf[1] = sz;
+    memcpy(&buf[2], &wdata[offs], sz);
+    dgw_cmd(buf, sizeof(buf), 2 + sz);
+
+    memcpy(&rdata[offs], &buf[1], sz);
+
+    size -= sz;
+    offs += sz;
+  }
+}
+
---
+++ embedded/spi_master.c
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2016, Alex Taradov <alex@taradov.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*- Includes ----------------------------------------------------------------*/
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "samd21.h"
+#include "hal_gpio.h"
+#include "spi_master.h"
+
+/*- Definitions -------------------------------------------------------------*/
+HAL_GPIO_PIN(MISO,            A, 4);
+HAL_GPIO_PIN(MOSI,            A, 6);
+HAL_GPIO_PIN(SCLK,            A, 7);
+HAL_GPIO_PIN(SS,              A, 5);
+#define SPI_SERCOM            SERCOM0
+#define SPI_SERCOM_PMUX       PORT_PMUX_PMUXE_D_Val
+#define SPI_SERCOM_GCLK_ID    SERCOM0_GCLK_ID_CORE
+#define SPI_SERCOM_CLK_GEN    0
+#define SPI_SERCOM_APBCMASK   PM_APBCMASK_SERCOM0
+
+/*- Implementations ---------------------------------------------------------*/
+
+//-----------------------------------------------------------------------------
+int spi_init(int freq)
+{
+  int baud = F_CPU / (2 * freq) - 1;
+
+  if (baud < 0)
+    baud = 0;
+
+  freq = F_CPU / (2 * (baud + 1));
+
+  HAL_GPIO_MISO_in();
+  HAL_GPIO_MISO_pmuxen(SPI_SERCOM_PMUX);
+
+  HAL_GPIO_MOSI_out();
+  HAL_GPIO_MOSI_pmuxen(SPI_SERCOM_PMUX);
+
+  HAL_GPIO_SCLK_out();
+  HAL_GPIO_SCLK_pmuxen(SPI_SERCOM_PMUX);
+
+  HAL_GPIO_SS_out();
+  HAL_GPIO_SS_set();
+
+  PM->APBCMASK.reg |= SPI_SERCOM_APBCMASK;
+
+  GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(SPI_SERCOM_GCLK_ID) |
+      GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN(SPI_SERCOM_CLK_GEN);
+
+  SPI_SERCOM->SPI.CTRLA.reg = SERCOM_SPI_CTRLA_SWRST;
+  while (SPI_SERCOM->SPI.CTRLA.reg & SERCOM_SPI_CTRLA_SWRST);
+
+  SPI_SERCOM->SPI.CTRLB.reg = SERCOM_SPI_CTRLB_RXEN;
+
+  SPI_SERCOM->SPI.BAUD.reg = baud;
+
+  SPI_SERCOM->SPI.CTRLA.reg = SERCOM_SPI_CTRLA_ENABLE |
+      SERCOM_SPI_CTRLA_DIPO(0) | SERCOM_SPI_CTRLA_DOPO(1) |
+      SERCOM_SPI_CTRLA_MODE_SPI_MASTER;
+
+  return freq;
+}
+
+//-----------------------------------------------------------------------------
+void spi_ss(int state)
+{
+  HAL_GPIO_SS_write(state);
+}
+
+//-----------------------------------------------------------------------------
+uint8_t spi_write_byte(uint8_t byte)
+{
+  SPI_SERCOM->SPI.DATA.reg = byte;
+  while (!SPI_SERCOM->SPI.INTFLAG.bit.RXC);
+  return SPI_SERCOM->SPI.DATA.reg;
+}
+
